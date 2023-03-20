5d8f4a7330ffd7737c3e4e7530fa98e1f8a12da6
Flavio Silva <55815265+201flaviosilva@users.noreply.github.com>
2023-03-20 22:04:05 +0000

    import all left minders

--- src/Minders.js
+++ src/Minders.js
@@ -4,7 +4,6 @@ import Autumn from "./assets/Minders/Autumn.png";
 import BarMinder from "./assets/Minders/BarMinder.png";
 import BatMinder from "./assets/Minders/BatMinder.png";
 import BigWally from "./assets/Minders/Big Wally.png";
-import BigodesRosa from "./assets/Minders/Bigodes Rosa.png";
 import Bigodes from "./assets/Minders/Bigodes.png";
 import Biker from "./assets/Minders/Biker.png";
 import BlackCatWhiteCat from "./assets/Minders/Black Cat White Cat.png";
@@ -37,6 +36,44 @@ import Minder from "./assets/Minders/Minder.png";
 import Mindera from "./assets/Minders/Mindera.png";
 import Minderonauta from "./assets/Minders/Minderonauta.png";
 import MinderPartyHat from "./assets/Minders/MinderPartyHat.svg";
+import MinderMario from "./assets/Minders/MindMario.png";
+import MindOwl from "./assets/Minders/MindOwl.png";
+import Mindswap from "./assets/Minders/Mindswap.png";
+import MRAnibal from "./assets/Minders/MR Anibal.png";
+import MRMinder from "./assets/Minders/MR Minder.png";
+import MSchool from "./assets/Minders/MSchool.jpg";
+import MudPink from "./assets/Minders/Mud Pink.png";
+import Mud from "./assets/Minders/Mud.png";
+import MustacheMinder from "./assets/Minders/Mustache Minder.png";
+import NeonBlue from "./assets/Minders/Neon Blue.png";
+import NeonPink from "./assets/Minders/Neon Pink.png";
+import OctopusMinder from "./assets/Minders/Octopus Minder.png";
+import Palhaco from "./assets/Minders/Palhaco.png";
+import PattyBooks from "./assets/Minders/Patty Books.png";
+import Patty from "./assets/Minders/Patty.png";
+import Pirata from "./assets/Minders/Pirata.png";
+import PixelMinder from "./assets/Minders/PixelMinder.png";
+import Play from "./assets/Minders/Play.png";
+import Pumpkin from "./assets/Minders/Pumpkin.png";
+import Reindeer from "./assets/Minders/Reindeer.png";
+import Rocket from "./assets/Minders/Rocket.png";
+import RodinhasNatal from "./assets/Minders/Rodinhas Natal.png";
+import Rodinhas from "./assets/Minders/Rodinhas.png";
+import RoundBlackOutline from "./assets/Minders/Round Black Outline.png";
+import RoundBlueGrid from "./assets/Minders/Round Blue Grid.png";
+import RoundGreenYellow from "./assets/Minders/Round Green Yellow.png";
+import RoundYellow from "./assets/Minders/Round Yellow.png";
+import Santa from "./assets/Minders/Santa.png";
+import Slime from "./assets/Minders/Slime.png";
+import SouthPark from "./assets/Minders/South Park.png";
+import SpacePatch from "./assets/Minders/Space Patch.png";
+import SpaceshipColor from "./assets/Minders/Spaceship Color.png";
+import TheHatefulEight from "./assets/Minders/The Hateful Eight.png";
+import TypographicLogo from "./assets/Minders/Typographic Logo.png";
+import VampireMinder from "./assets/Minders/Vampire Minder.png";
+import WineMinder from "./assets/Minders/Wine Minder.png";
+import Winter1 from "./assets/Minders/Winter 1.png";
+import Winter2 from "./assets/Minders/Winter 2.png";
 
 const Minders = [
 	[{ name: "Agent", src: Agent, }],
@@ -47,7 +84,7 @@ const Minders = [
 	[{ name: "Big Wally", src: BigWally, }],
 	[
 		{ name: "Bigodes", src: Bigodes, },
-		{ name: "Bigodes Rosa", src: BigodesRosa, }
+		{ name: "Mustache Minder", src: MustacheMinder, }
 	],
 	[{ name: "Biker", src: Biker, }],
 	[{ name: "Black and White Cat", src: BlackCatWhiteCat, }],
@@ -59,6 +96,7 @@ const Minders = [
 		{ name: "Christmas 2", src: Christmas2, },
 		{ name: "Christmas 3", src: Christmas3, },
 		{ name: "Christmas 4", src: Christmas4, },
+		{ name: "Santa", src: Santa, },
 	],
 	[{ name: "CoimbraTraje", src: CoimbraTraje, }],
 	[{ name: "Cookie", src: Cookie, }],
@@ -72,7 +110,10 @@ const Minders = [
 		{ name: "GirlMinder", src: GirlMinder, },
 	],
 	[{ name: "Fuzzy", src: Fuzzy, }],
-	[{ name: "Gamer", src: Gamer, }],
+	[
+		{ name: "Gamer", src: Gamer, },
+		{ name: "Play", src: Play, },
+	],
 	[
 		{ name: "Gentlemen", src: Gentlemen, },
 		{ name: "Gentleman", src: Gentleman, },
@@ -85,9 +126,58 @@ const Minders = [
 	],
 	[{ name: "Mental Health", src: MentalHealth, }],
 	[{ name: "Minder", src: Minder, }],
-	[{ name: "Mindera Banner", src: Mindera, }],
+	[
+		{ name: "Mindera Banner", src: Mindera, },
+		{ name: "Typographic Logo", src: TypographicLogo, },
+	],
 	[{ name: "Minderonauta", src: Minderonauta, }],
 	[{ name: "MinderPartyHat", src: MinderPartyHat, description: "Minder 5th Birthday Tee - MinderPartyHat" }],
+	[{ name: "Mind Mario", src: MinderMario, }],
+	[{ name: "Mind Owl", src: MindOwl, }],
+	[{ name: "Mindswap", src: Mindswap, }],
+	[{ name: "MR Anibal", src: MRAnibal, }],
+	[{ name: "MR Minder", src: MRMinder, }],
+	[{ name: "M School", src: MSchool, }],
+	[
+		{ name: "Mud", src: Mud, },
+		{ name: "Mud Pink", src: MudPink, },
+	],
+	[
+		{ name: "Neon Blue", src: NeonBlue, },
+		{ name: "Neon Pink", src: NeonPink, },
+	],
+	[{ name: "Octopus Minder", src: OctopusMinder, }],
+	[{ name: "Palhaço", src: Palhaco, }],
+	[
+		{ name: "Patty", src: Patty, },
+		{ name: "Patty Books", src: PattyBooks, },
+	],
+	[{ name: "Pixel Minder", src: PixelMinder, }],
+	[{ name: "Pirata", src: Pirata, }],
+	[{ name: "Pumpkin", src: Pumpkin, }],
+	[{ name: "Reindeer", src: Reindeer, }],
+	[{ name: "Rocket", src: Rocket, }],
+	[
+		{ name: "Rodinhas", src: Rodinhas, },
+		{ name: "Rodinhas Natal", src: RodinhasNatal, },
+	],
+	[
+		{ name: "Round Black Outline", src: RoundBlackOutline, },
+		{ name: "Round Blue Grid", src: RoundBlueGrid, },
+		{ name: "Round Green Yellow", src: RoundGreenYellow, },
+		{ name: "Round Yellow", src: RoundYellow, },
+	],
+	[{ name: "Slime", src: Slime, }],
+	[{ name: "SouthPark", src: SouthPark, }],
+	[{ name: "SpacePatch", src: SpacePatch, }],
+	[{ name: "SpaceshipColor", src: SpaceshipColor, }],
+	[{ name: "TheHatefulEight", src: TheHatefulEight, }],
+	[{ name: "VampireMinder", src: VampireMinder, }],
+	[{ name: "WineMinder", src: WineMinder, }],
+	[
+		{ name: "Winter 1", src: Winter1, },
+		{ name: "Winter 2", src: Winter2, },
+	],
 ];
 
 export default Minders;
